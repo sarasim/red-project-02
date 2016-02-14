644079b6ab4b5c666020a0e6945c0da0a7cde166
Alan Chow <hello@alanchow.ca>
2016-02-14 13:35:55 -0800

    update to include loader gif; fix gulpfile

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 $(function(){
+  $('.loader-gif').hide();
 
-$('button').on('click', function(event){
-  event.preventDefault();
+  $('button').on('click', function(event){
+    event.preventDefault();
 
     var hashtag = $('input[name="hashtag"]').val();
     var $photoGrid = $('.list');
     var list = '';
 
-$('.site-header').css('height', 'auto');
-
+    $('.site-header').css('height', 'auto');
+    $('.loader-gif').show();
     $.ajax({
       dataType: 'jsonp',
       method: 'GET',
       url: 'https://api.instagram.com/v1/tags/'+ hashtag +'/media/recent?count=12&client_id=71e21c4bf4294a8498860283067eb682'
     })
     .done(function(instaData){
-      console.log(instaData);
 
       $.each(instaData.data, function(index, value){
         list += '<li class="photo-wrapper">';
-        list += '<img src=' +value.images.standard_resolution.url +'>';
+        list += '<a href=' + value.link + ' target="_blank"><img src=' +value.images.standard_resolution.url +'></a>';
         list +=     '<div class="meta-data">';
         list +=         '<div class="user-wrapper"><img src=' +value.user.profile_picture+ '>';
         list +=         '</div>';
@@ -32,11 +32,11 @@ $('.site-header').css('height', 'auto');
       });
 
       $photoGrid.empty().append(list);
-        list = '';
+      $('.loader-gif').hide();
+      list = '';
     });
-
   });
 
-//end document
+  //end document
 
 });
